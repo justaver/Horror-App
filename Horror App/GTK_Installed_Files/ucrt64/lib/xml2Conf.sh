d943486b99534ab8df8e1d0658e32df7a67b9847
justaver <85198736+justaver@users.noreply.github.com>
2024-08-08 18:15:30 -0400

    [FEAT] gtk setup properly

--- Horror App/GTK_Installed_Files/ucrt64/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/ucrt64/lib"
-XML2_LIBS="-lxml2 -L/ucrt64/lib -lz -L/ucrt64/lib -llzma   -liconv   "
-XML2_INCLUDEDIR="-I/ucrt64/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-
--- Horror App/GTK_Installed_Files/ucrt64/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/ucrt64/lib"
-XSLT_LIBS="-lxslt -L/ucrt64/lib -lxml2 -L/ucrt64/lib -lz -L/ucrt64/lib -llzma -liconv -lws2_32 "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/ucrt64/include"
-MODULE_VERSION="xslt-1.1.34"
